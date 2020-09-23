390470cf6638ad718aa3862df7f1581601d1d3ab
NOUADRA <46531152+NOUADRA@users.noreply.github.com>
2020-09-23 17:25:42 -0400

    instance canard
    instance canard

---
+++ compute.tf
@@ -0,0 +1,22 @@
+resource "google_compute_instance" "canard" {
+  name         = "canard"
+  machine_type = "f1-micro"
+  zone         = "us-east1-c"
+  tags         = ["public"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    subnetwork = google_compute_subnetwork.mtl-dmz.name
+    access_config {
+
+    }
+  }
+
+
+  metadata_startup_script = "apt-get -y update && apt-get -y upgrade && apt-get -y install apache2 && systemctl start apache2"
+}
