76d2ac9f05f21e7f0ddd22bb68ef7836d341e86c
NOUADRA MOHAMED <46531152+NOUADRA@users.noreply.github.com>
2020-09-24 12:04:45 -0400

    Update compute.tf
    dmz

--- compute.tf
+++ compute.tf
@@ -8,13 +8,18 @@ resource "google_compute_instance" "canard" {
     initialize_params {
       image = "debian-cloud/debian-9"
   }
-  }
+  resource "google_compute_network" "devoir1" {
+  name                    = "devoir1"
+  auto_create_subnetworks = "false"
+}
 
-  network_interface {
-    subnetwork = google_compute_subnetwork.mtl-dmz.name
-    access_config {
 
-    }
+resource "google_compute_subnetwork" "prod-interne" {
+  name          = "prod-interne"
+  ip_cidr_range = "10.0.3.0/24"
+  region        = "us-east1"
+  network       = google_compute_network.devoir1.self_link
+}
   }
 
   metadata_startup_script = "apt-get -y update && apt-get -y upgrade && apt-get -y install apache2 && systemctl start apache2"
